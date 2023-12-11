758883f949ffaa450be6a7239262a763cca8d27c
Yihun Alemayehu <yankure01@gmail.com>
2023-12-11 10:18:33 +0300

    fdb-07 commit

---
+++ lib/logic/cubit/internet_state.dart
@@ -0,0 +1,10 @@
+part of 'internet_cubit.dart';
+
+sealed class InternetState extends Equatable {
+  const InternetState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class InternetInitial extends InternetState {}
