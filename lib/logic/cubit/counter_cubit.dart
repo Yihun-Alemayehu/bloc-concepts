a1cfd0c31a26f14f6a4fdc96453ac5744f093844
Yihun Alemayehu <yankure01@gmail.com>
2023-12-11 11:43:27 +0300

    fdb-09 commit

--- lib/logic/cubit/counter_cubit.dart
+++ lib/logic/cubit/counter_cubit.dart
@@ -1,12 +1,32 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
+import 'package:bloc_concepts/logic/cubit/internet_cubit.dart';
+import 'package:bloc_concepts/presentation/constants/enum.dart';
 import 'package:equatable/equatable.dart';
 
 part 'counter_state.dart';
 
 class CounterCubit extends Cubit<CounterState> {
-  CounterCubit() : super(CounterState(counterValue: 0, wasIncremented: false));
+  final InternetCubit internetCubit;
+  late StreamSubscription internetStreamSubscription;
 
-  void increment() => emit(CounterState(counterValue: state.counterValue + 1, wasIncremented: true));
+  CounterCubit({required this.internetCubit})
+      : super(CounterState(counterValue: 0, wasIncremented: false)) {
+    internetStreamSubscription = internetCubit.listen((internetState) {
+      if (internetState is InternetConnected &&
+          internetState.connectionType == ConnectionType.wifi) {
+        increment();
+      }else if (internetState is InternetConnected && 
+      internetState.connectionType == ConnectionType.data){
+        decrement();
+      }
+    });
+  }
 
-  void decrement() => emit(CounterState(counterValue: state.counterValue - 1, wasIncremented: false));
+  void increment() => emit(
+      CounterState(counterValue: state.counterValue + 1, wasIncremented: true));
+
+  void decrement() => emit(CounterState(
+      counterValue: state.counterValue - 1, wasIncremented: false));
 }
--- lib/logic/cubit/internet_cubit.dart
+++ lib/logic/cubit/internet_cubit.dart
@@ -1,9 +1,43 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:bloc_concepts/presentation/constants/enum.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
 
 part 'internet_state.dart';
 
 class InternetCubit extends Cubit<InternetState> {
-  InternetCubit() : super(InternetLoading());
+  final Connectivity connectivity;
+  late StreamSubscription connectivityStreamSubscription;
+  InternetCubit({
+    required this.connectivity,
+  }) : super(InternetLoading()) {
+    connectivityStreamSubscription =
+        connectivity.onConnectivityChanged.listen((connectivityResult) {
+      if (connectivityResult == ConnectivityResult.wifi) {
+        emitInternetConnected(ConnectionType.wifi);
+      } else if (connectivityResult == ConnectivityResult.mobile) {
+        emitInternetConnected(ConnectionType.data);
+      }
+    });
+  }
+
+  void emitInternetConnected(ConnectionType _connectionType) => emit(
+        InternetConnected(connectionType: _connectionType),
+      );
+
+  void emitInternetDisconnected(ConnectionType _connectionType) => emit(
+        InternetConnected(connectionType: _connectionType),
+      );
+
+  @override
+  Future<void> close() {
+    connectivityStreamSubscription.cancel();
+    return super.close();
+  }
+
+  StreamSubscription listen(Null Function(dynamic internetState) param0) {
+  }
 }
