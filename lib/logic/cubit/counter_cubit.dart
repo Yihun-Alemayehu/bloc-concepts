fcafde7ec2f2ed8d56303555288cd6c4d7bed340
Yihun Alemayehu <yankure01@gmail.com>
2024-01-13 11:02:14 +0300

    fdb-03

--- lib/logic/cubit/counter_cubit.dart
+++ lib/logic/cubit/counter_cubit.dart
@@ -1,12 +1,36 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
+import 'package:bloc_concepts/logic/cubit/internet_cubit.dart';
+import 'package:bloc_concepts/presentation/constants/enum.dart';
 part 'counter_state.dart';
 
 class CounterCubit extends Cubit<CounterState> {
-  CounterCubit(): super(CounterState(counterValue: 0, wasIncremented: false));
+  final InternetCubit internetCubit;
+  late StreamSubscription internetStreamSubscription;
+
+  CounterCubit({
+    required this.internetCubit,
+    // required this.internetStreamSubscription,
+  }): super(CounterState(counterValue: 0, wasIncremented: false)){
+    internetStreamSubscription = internetCubit.stream.listen((internetState){
+      if(internetState is InternetConnected && internetState.connectionType == ConnectionType.wifi) {
+        increment();
+      }else if(internetState is InternetConnected && internetState.connectionType == ConnectionType.Mobile){
+        decrement();
+      }
+    });
+  }
   void increment() => emit(
       CounterState(counterValue: state.counterValue + 1, wasIncremented: true));
 
   void decrement() => emit(CounterState(
       counterValue: state.counterValue - 1, wasIncremented: false));
+
+  @override
+  Future<void> close(){
+    internetStreamSubscription.cancel();
+    return super.close();
+  }
 
 }
--- lib/logic/cubit/internet_cubit.dart
+++ lib/logic/cubit/internet_cubit.dart
@@ -1,9 +1,49 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
-import 'package:bloc_concepts/presentation/constants/enum.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:equatable/equatable.dart';
+
+import 'package:bloc_concepts/presentation/constants/enum.dart';
 
 part 'internet_state.dart';
 
 class InternetCubit extends Cubit<InternetState> {
-  InternetCubit() : super(InternetLoading());
+  final Connectivity connectivity;
+  StreamSubscription connectivityStreamSubscription;
+
+  InternetCubit({
+    required this.connectivity,
+    required this.connectivityStreamSubscription,
+  }) : super(InternetLoading()){
+    connectivityStreamSubscription = 
+      connectivity.onConnectivityChanged.listen((event) {
+        if(event == ConnectivityResult.wifi){
+          emitInternetConnected(ConnectionType.wifi);
+        }else if(event == ConnectivityResult.mobile){
+          emitInternetConnected(ConnectionType.Mobile);
+        }else if(event == ConnectivityResult.none){
+          emitInternetDisconnected();
+        }
+      });
+  }
+
+  void emitInternetConnected(ConnectionType _connectionType) => emit(
+        InternetConnected(connectionType: _connectionType),
+      );
+
+  void emitInternetDisconnected() => emit(
+        InternetDisconnected(),
+      );
+
+  @override
+  Future<void> close(){
+    connectivityStreamSubscription.cancel();
+    return super.close();
+  }
+
+  // StreamSubscription listen(Null Function(dynamic internetState) param0) {
+  //   return InternetState;
+  // }
 }
